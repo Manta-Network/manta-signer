c928011e495ade895854739f918cfa756e4246de
stechu <shumo.chu@gmail.com>
2021-07-23 00:40:24 -0400

    fmt

--- lib/zkp/src/lib.rs
+++ lib/zkp/src/lib.rs
@@ -3,7 +3,13 @@ extern crate libc;
 use std::ffi::CString;
 
 #[no_mangle]
-pub extern "C" fn generate_transfer(_app_version: *const libc::c_char, _buffer: *const libc::c_char, _len: libc::size_t, _out: *mut *mut libc::c_char, out_len: *mut libc::size_t) -> libc::c_int {
+pub extern "C" fn generate_transfer(
+    _app_version: *const libc::c_char,
+    _buffer: *const libc::c_char,
+    _len: libc::size_t,
+    _out: *mut *mut libc::c_char,
+    out_len: *mut libc::size_t,
+) -> libc::c_int {
     unsafe {
         let a = String::from("0xgenerate_transfer");
         let len = a.len();
@@ -15,7 +21,13 @@ pub extern "C" fn generate_transfer(_app_version: *const libc::c_char, _buffer: 
 }
 
 #[no_mangle]
-pub extern "C" fn generate_reclaim(_app_version: *const libc::c_char, _name: *const libc::c_char, _len: libc::size_t, _out: *mut *mut libc::c_char, out_len: *mut libc::size_t) -> libc::c_int {
+pub extern "C" fn generate_reclaim(
+    _app_version: *const libc::c_char,
+    _name: *const libc::c_char,
+    _len: libc::size_t,
+    _out: *mut *mut libc::c_char,
+    out_len: *mut libc::size_t,
+) -> libc::c_int {
     unsafe {
         let a = String::from("0xgenerate_reclaim");
         let len = a.len();
