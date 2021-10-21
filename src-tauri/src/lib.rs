c59ab4a0b14b286611bda20ebd23e22eacbccbb0
Brandon H. Gomes <bhgomes@pm.me>
2021-10-21 17:59:44 -0400

    feat: add HTTP server to connect with frontend

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,604 @@
+// Copyright 2019-2021 Manta Network.
+// This file is part of manta-signer.
+//
+// manta-signer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// manta-signer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with manta-signer. If not, see <http://www.gnu.org/licenses/>.
+
+//! Manta Signer Server
+
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
+#![forbid(rustdoc::broken_intra_doc_links)]
+#![forbid(missing_docs)]
+
+use ark_serialize::CanonicalDeserialize;
+use async_std::{
+    fs::{self, File},
+    io::{self, ReadExt, WriteExt},
+};
+use bip0039::{Count, Mnemonic};
+use cocoon::{Cocoon, Error as CocoonError};
+use codec::{Decode, Encode};
+use core::convert::TryInto;
+use manta_api::{
+    DeriveShieldedAddressParams, GenerateAssetParams, GeneratePrivateTransferBatchParams,
+    GenerateReclaimBatchParams, MantaRootSeed, RecoverAccountParams,
+};
+use manta_crypto::{Groth16Pk, MantaSerDes};
+use rand::{thread_rng, SeedableRng};
+use rand_chacha::ChaCha20Rng;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tauri::{
+    async_runtime::{channel, Mutex, Receiver, RwLock, Sender},
+    Window,
+};
+use tide::{
+    listener::ToListener, Body, Error, Request as ServerRequest, Result as ServerResult, Server,
+    StatusCode,
+};
+
+/// Ensure that `$expr` is `Ok(_)` and if not returns a [`StatusCode::InternalServerError`].
+macro_rules! ensure {
+    ($expr:expr) => {
+        ensure!($expr, "")
+    };
+    ($expr:expr, $msg:expr) => {
+        core::result::Result::map_err($expr, move |_| {
+            Error::from_str(StatusCode::InternalServerError, $msg)
+        })
+    };
+}
+
+/// Password
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Password {
+    /// Known Password
+    Known(String),
+
+    /// Unknown Password
+    Unknown,
+}
+
+impl Password {
+    /// Returns the inner password, if it is known.
+    #[inline]
+    pub fn known(self) -> Option<String> {
+        match self {
+            Self::Known(password) => Some(password),
+            _ => None,
+        }
+    }
+}
+
+impl Default for Password {
+    #[inline]
+    fn default() -> Self {
+        Self::Unknown
+    }
+}
+
+/// Password Storage Type
+type PasswordStoreType = Arc<RwLock<Option<Sender<Password>>>>;
+
+/// Password Storage Handle
+pub struct PasswordStoreHandle(PasswordStoreType);
+
+impl PasswordStoreHandle {
+    /// Builds a new password storage system, waiting on the receiver to receive it's first
+    /// message.
+    #[inline]
+    pub async fn setup(self) -> Receiver<Password> {
+        let (sender, mut receiver) = channel(1);
+        *self.0.write().await = Some(sender);
+        receiver.recv().await;
+        receiver
+    }
+}
+
+/// Password Storage
+#[derive(Default)]
+pub struct PasswordStore(PasswordStoreType);
+
+impl PasswordStore {
+    /// Returns a handle for setting up a [`PasswordStore`].
+    #[inline]
+    pub fn handle(&self) -> PasswordStoreHandle {
+        PasswordStoreHandle(self.0.clone())
+    }
+
+    /// Loads a new password into the state.
+    #[inline]
+    pub async fn load(&self, password: String) {
+        if let Some(state) = &*self.0.read().await {
+            state.send(Password::Known(password)).await.unwrap();
+        }
+    }
+
+    /// Clears the password state.
+    #[inline]
+    pub async fn clear(&self) {
+        if let Some(state) = &*self.0.read().await {
+            state.send(Password::Unknown).await.unwrap();
+        }
+    }
+}
+/// Root Seed
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct RootSeed(pub MantaRootSeed);
+
+impl RootSeed {
+    /// Builds a new [`RootSeed`] from a `mnemonic`.
+    #[inline]
+    pub fn new(mnemonic: &Mnemonic) -> Self {
+        Self(mnemonic.to_seed(""))
+    }
+
+    /// Saves `self` to the standard root seed file, encrypting it with `password`.
+    #[inline]
+    pub async fn save(self, password: String) -> Result<(), CocoonError> {
+        let mut data = Vec::new();
+        Cocoon::new(password.as_bytes()).dump(self.0.to_vec(), &mut data)?;
+        File::create("root_seed.aes")
+            .await
+            .map_err(CocoonError::Io)?
+            .write_all(&data)
+            .await
+            .map_err(CocoonError::Io)
+    }
+
+    /// Loads `self` from the standard root seed file, decrypting it with `password`.
+    #[inline]
+    pub async fn load(password: String) -> Result<Self, CocoonError> {
+        let mut data = Vec::new();
+        File::open("root_seed.aes")
+            .await
+            .map_err(CocoonError::Io)?
+            .read_to_end(&mut data);
+        Ok(Self(
+            Cocoon::new(password.as_bytes())
+                .parse(&mut data.as_slice())?
+                .try_into()
+                .expect(""),
+        ))
+    }
+}
+
+impl Default for RootSeed {
+    #[inline]
+    fn default() -> Self {
+        Self([Default::default(); 64])
+    }
+}
+
+/// Checks if a root seed exists at the canonical file path.
+#[inline]
+pub async fn account_exists() -> io::Result<bool> {
+    Ok(fs::metadata("root_seed.aes").await?.is_file())
+}
+
+/// Creates a new account by building and saving a new root seed from the given `password`.
+#[inline]
+pub async fn create_account(password: String) -> Result<Mnemonic, CocoonError> {
+    let mnemonic = Mnemonic::generate(Count::Words12);
+    RootSeed::new(&mnemonic).save(password).await?;
+    Ok(mnemonic)
+}
+
+/// Loads proving key from `path`.
+#[inline]
+pub async fn load_proving_key(path: &str) -> Groth16Pk {
+    Groth16Pk::deserialize_unchecked(
+        fs::read(path)
+            .await
+            .expect("Failed to read file.")
+            .as_slice(),
+    )
+    .expect("Failed to deserialize proving key.")
+}
+
+/// Transaction Type
+#[derive(Deserialize, Serialize)]
+pub enum TransactionType {
+    /// Private Transfer
+    PrivateTransfer {
+        /// Recipient Address
+        recipient: String,
+    },
+
+    /// Reclaim
+    Reclaim,
+}
+
+/// Transaction Summary
+#[derive(Deserialize, Serialize)]
+pub struct TransactionSummary {
+    /// Transaction Type
+    pub transaction: TransactionType,
+
+    /// Transaction Amount
+    pub amount: String,
+
+    /// Currency Symbol
+    pub currency_symbol: String,
+}
+
+/// Inner State
+struct InnerState {
+    /// Main Window
+    window: Window,
+
+    /// Password Receiver
+    password: Receiver<Password>,
+
+    /// Current Root Seed
+    root_seed: Option<RootSeed>,
+}
+
+impl InnerState {
+    /// Builds a new [`InnerState`] from `window` and `password`.
+    #[inline]
+    fn new(window: Window, password: Receiver<Password>) -> Self {
+        Self {
+            window,
+            password,
+            root_seed: None,
+        }
+    }
+
+    /// Returns the password from the user, prompted with `prompt`.
+    #[inline]
+    async fn authorize<T>(&mut self, prompt: T) -> Option<Password>
+    where
+        T: Serialize,
+    {
+        self.window.emit("authorize", prompt).unwrap();
+        self.window.show().unwrap();
+        self.password.recv().await
+    }
+
+    /// Sets the inner seed from the output of a call to [`Self::authorize`] using the given
+    /// `prompt`.
+    #[inline]
+    async fn set_seed_from_authorization<T>(&mut self, prompt: T) -> Option<()>
+    where
+        T: Serialize,
+    {
+        if let Password::Known(password) = self.authorize(prompt).await? {
+            self.root_seed = RootSeed::load(password).await.ok();
+        }
+        Some(())
+    }
+
+    /// Returns the stored root seed if it exists, otherwise, gets the password from the user
+    /// and tries to decrypt the root seed.
+    #[inline]
+    async fn get_root_seed<T>(&mut self, prompt: T) -> Option<RootSeed>
+    where
+        T: Serialize,
+    {
+        if self.root_seed.is_none() {
+            self.set_seed_from_authorization(prompt).await?;
+        }
+        self.root_seed
+    }
+
+    /// Returns the currently stored root seed if it matches the one returned by the user after
+    /// prompting.
+    #[inline]
+    async fn check_root_seed<T>(&mut self, prompt: T) -> Option<RootSeed>
+    where
+        T: Serialize,
+    {
+        match self.root_seed {
+            Some(current_root_seed) => {
+                let password = self.authorize(prompt).await?.known()?;
+                if current_root_seed == RootSeed::load(password).await.ok()? {
+                    Some(current_root_seed)
+                } else {
+                    None
+                }
+            }
+            _ => {
+                self.set_seed_from_authorization(prompt).await?;
+                self.root_seed
+            }
+        }
+    }
+}
+
+/// Signer State
+#[derive(Clone)]
+pub struct State(Arc<Mutex<InnerState>>);
+
+impl State {
+    /// Builds a new [`State`] using `window` and `password`.
+    #[inline]
+    pub fn new(window: Window, password: Receiver<Password>) -> Self {
+        Self(Arc::new(Mutex::new(InnerState::new(window, password))))
+    }
+
+    /// Returns the stored root seed if it exists, otherwise, gets the password from the user
+    /// and tries to decrypt the root seed.
+    #[inline]
+    async fn get_root_seed<T>(&self, prompt: T) -> Option<RootSeed>
+    where
+        T: Serialize,
+    {
+        self.0.lock().await.get_root_seed(prompt).await
+    }
+
+    /// Returns the currently stored root seed if it matches the one returned by the user after
+    /// prompting.
+    #[inline]
+    async fn check_root_seed<T>(&self, prompt: T) -> Option<RootSeed>
+    where
+        T: Serialize,
+    {
+        self.0.lock().await.check_root_seed(prompt).await
+    }
+}
+
+/// Server Request Type
+pub type Request = ServerRequest<State>;
+
+/// Signer Service
+pub struct Service(Server<State>);
+
+impl Service {
+    /// Builds a new [`Service`] from `window` and `password`.
+    #[inline]
+    pub fn build(window: Window, password: Receiver<Password>) -> Self {
+        let mut server = Server::with_state(State::new(window, password));
+        server.at("/heartbeat").get(Self::heartbeat);
+        server.at("/recoverAccount").post(Self::recover_account);
+        server
+            .at("/deriveShieldedAddress")
+            .post(Self::derive_shielded_address);
+        server.at("/generateAsset").post(Self::generate_asset);
+        server.at("/generateMintData").post(Self::mint);
+        server
+            .at("/generatePrivateTransferData")
+            .post(Self::private_transfer);
+        server.at("/generateReclaimData").post(Self::reclaim);
+        Self(server)
+    }
+
+    /// Starts the service on `listener`.
+    #[inline]
+    pub async fn serve<L>(self, listener: L) -> io::Result<()>
+    where
+        L: ToListener<State>,
+    {
+        self.0.listen(listener).await
+    }
+
+    /// Sends a heartbeat to the client.
+    #[inline]
+    async fn heartbeat(request: Request) -> ServerResult<String> {
+        let _ = request;
+        Ok(String::from("heartbeat"))
+    }
+
+    /// Runs an account recovery for the given `request`.
+    #[inline]
+    async fn recover_account(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(RecoverAccountParams::decode(&mut body.as_slice()))?;
+        let root_seed = ensure!(state.get_root_seed("recover_account").await.ok_or(()))?;
+        let recovered_account = manta_api::recover_account(params, &root_seed.0).encode();
+        Ok(Body::from_json(&RecoverAccountMessage::new(recovered_account))?.into())
+    }
+
+    /// Generates a new derived shielded address for the given `request`.
+    #[inline]
+    async fn derive_shielded_address(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(DeriveShieldedAddressParams::decode(&mut body.as_slice(),))?;
+        let root_seed = ensure!(state
+            .get_root_seed("derive_shielded_address")
+            .await
+            .ok_or(()))?;
+        let mut address = Vec::new();
+        ensure!(manta_api::derive_shielded_address(params, &root_seed.0).serialize(&mut address))?;
+        Ok(Body::from_json(&ShieldedAddressMessage::new(address))?.into())
+    }
+
+    /// Generates an asset for the given `request`.
+    #[inline]
+    async fn generate_asset(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(GenerateAssetParams::decode(&mut body.as_slice()))?;
+        let root_seed = ensure!(state.get_root_seed("generate_asset").await.ok_or(()))?;
+        let asset = manta_api::generate_signer_input_asset(params, &root_seed.0).encode();
+        Ok(Body::from_json(&AssetMessage::new(asset))?.into())
+    }
+
+    /// Generates mint data for the given `request`.
+    #[inline]
+    async fn mint(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(GenerateAssetParams::decode(&mut body.as_slice()))?;
+        let root_seed = ensure!(state.get_root_seed("mint").await.ok_or(()))?;
+        let mut mint_data = Vec::new();
+        ensure!(manta_api::generate_mint_data(params, &root_seed.0).serialize(&mut mint_data))?;
+        Ok(Body::from_json(&MintMessage::new(mint_data))?.into())
+    }
+
+    /// Generates private transfer data for the given `request`.
+    #[inline]
+    async fn private_transfer(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(GeneratePrivateTransferBatchParams::decode(
+            &mut body.as_slice()
+        ))?;
+        let root_seed = ensure!(state.check_root_seed("private_transfer").await.ok_or(()))?;
+        let private_transfer_data = manta_api::batch_generate_private_transfer_data(
+            params,
+            &root_seed.0,
+            "",
+            &mut ChaCha20Rng::from_rng(thread_rng()).expect("Unable to sample RNG."),
+        )
+        .encode();
+        Ok(Body::from_json(&PrivateTransferMessage::new(private_transfer_data))?.into())
+    }
+
+    /// Generates reclaim data for the given `request`.
+    #[inline]
+    async fn reclaim(mut request: Request) -> ServerResult {
+        let (body, state) = Self::process(&mut request).await?;
+        let params = ensure!(GenerateReclaimBatchParams::decode(&mut body.as_slice()))?;
+        let root_seed = ensure!(state.check_root_seed("private_transfer").await.ok_or(()))?;
+        let reclaim_data = manta_api::batch_generate_reclaim_data(
+            params,
+            &root_seed.0,
+            "",
+            "",
+            &mut ChaCha20Rng::from_rng(thread_rng()).expect("Unable to sample RNG."),
+        )
+        .encode();
+        Ok(Body::from_json(&ReclaimMessage::new(reclaim_data))?.into())
+    }
+
+    /// Preprocesses a `request`, extracting the body as a byte vector and returning the
+    /// internal state.
+    #[inline]
+    async fn process(request: &mut Request) -> ServerResult<(Vec<u8>, &State)> {
+        Ok((request.body_bytes().await?, request.state()))
+    }
+}
+
+/// Shielded Address Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ShieldedAddressMessage {
+    /// Address
+    pub address: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl ShieldedAddressMessage {
+    /// Builds a new [`ShieldedAddressMessage`].
+    #[inline]
+    pub fn new(address: Vec<u8>) -> Self {
+        Self {
+            address,
+            version: "0.0.0".into(),
+        }
+    }
+}
+
+/// Recover Account Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct RecoverAccountMessage {
+    /// Recovered Account
+    pub recovered_account: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl RecoverAccountMessage {
+    /// Builds a new [`RecoverAccountMessage`].
+    #[inline]
+    pub fn new(recovered_account: Vec<u8>) -> Self {
+        Self {
+            recovered_account,
+            version: "0.0.0".into(),
+        }
+    }
+}
+
+/// Asset Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AssetMessage {
+    /// Asset
+    pub asset: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl AssetMessage {
+    /// Builds a new [`AssetMessage`].
+    #[inline]
+    pub fn new(asset: Vec<u8>) -> Self {
+        Self {
+            asset,
+            version: "0.0.0".into(),
+        }
+    }
+}
+
+/// Mint Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct MintMessage {
+    /// Mint Data
+    pub mint_data: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl MintMessage {
+    /// Builds a new [`MintMessage`].
+    #[inline]
+    pub fn new(mint_data: Vec<u8>) -> Self {
+        Self {
+            mint_data,
+            version: "0.0.0".into(),
+        }
+    }
+}
+
+/// Private Transfer Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct PrivateTransferMessage {
+    /// Private Transfer Data
+    pub private_transfer_data: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl PrivateTransferMessage {
+    /// Builds a new [`PrivateTransferMessage`].
+    #[inline]
+    pub fn new(private_transfer_data: Vec<u8>) -> Self {
+        Self {
+            private_transfer_data,
+            version: "0.0.0".into(),
+        }
+    }
+}
+
+/// Reclaim Message
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ReclaimMessage {
+    /// Reclaim Data
+    pub reclaim_data: Vec<u8>,
+
+    /// Version
+    pub version: String,
+}
+
+impl ReclaimMessage {
+    /// Builds a new [`ReclaimMessage`].
+    #[inline]
+    pub fn new(reclaim_data: Vec<u8>) -> Self {
+        Self {
+            reclaim_data,
+            version: "0.0.0".into(),
+        }
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,125 +19,29 @@
     windows_subsystem = "windows"
 )]
 
-use async_fs::{self as fs, File};
-use bip0039::{Count, Mnemonic};
-use codec::{Decode, Encode};
-use manta_crypto::MantaSerDes;
-use rand::{thread_rng, RngCore, SeedableRng};
-use rand_chacha::ChaCha20Rng;
+use manta_signer::{account_exists, create_account, PasswordStore, Service};
 use serde::{Deserialize, Serialize};
-use std::{io, sync::Arc};
 use tauri::{
-    async_runtime::{channel, RwLock, Sender},
-    CustomMenuItem, Manager, State, SystemTray, SystemTrayEvent, SystemTrayMenu, Window,
+    async_runtime::spawn, CustomMenuItem, Manager, State, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, Window,
 };
-
-/// Checks if a root seed exists at the canonical file path.
-#[inline]
-async fn account_exists() -> io::Result<bool> {
-    Ok(fs::metadata("root_seed.aes").await?.is_file())
-}
-
-/// Creates a new account by building and saving a new root seed from the given `password`.
-#[inline]
-async fn create_account(password: String) -> Option<Mnemonic> {
-    let mnemonic = Mnemonic::generate(Count::Words12);
-    manta_api::save_root_seed(mnemonic.to_seed(""), password).ok()?;
-    Some(mnemonic)
-}
-
-/// Transaction Type
-#[derive(Deserialize, Serialize)]
-enum TransactionType {
-    PrivateTransfer { recipient: String },
-    Reclaim,
-}
-
-/// Transaction Summary
-#[derive(Deserialize, Serialize)]
-struct TransactionSummary {
-    transaction: TransactionType,
-    amount: String,
-    currency_symbol: String,
-}
-
-/// Loads a transaction request from the wallet frontend.
-#[inline]
-async fn load_request() -> Option<TransactionSummary> {
-    // TODO: Load request from wallet frontend.
-    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-    Some(TransactionSummary {
-        transaction: TransactionType::Reclaim,
-        amount: String::from(""),
-        currency_symbol: String::from(""),
-    })
-}
-
-/// Sends an authorized proof payload to the wallet frontend.
-#[inline]
-async fn send_authorized(payload: ()) {
-    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-}
-
-/// Sends a decline notice to the wallet fronend.
-#[inline]
-async fn send_declined() {
-    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-}
-
-/// Generates ZKP proof payload to send to wallet frontend.
-#[inline]
-fn generate_proof_payload(password: String) -> () {
-    std::thread::sleep(std::time::Duration::from_secs(10))
-}
-
-/// Password State Type
-type PasswordStateType = Arc<RwLock<Option<Sender<Option<String>>>>>;
-
-/// Password State
-#[derive(Default)]
-struct Password(PasswordStateType);
-
-impl Password {
-    /// Builds a new state from an empty `state` and `sender`.
-    #[inline]
-    async fn setup(state: PasswordStateType, sender: Sender<Option<String>>) {
-        *state.write().await = Some(sender);
-    }
-
-    /// Loads a new password into the state.
-    #[inline]
-    async fn load(&self, password: String) {
-        if let Some(state) = &*self.0.read().await {
-            state.send(Some(password)).await.unwrap();
-        }
-    }
-
-    /// Clears the password state.
-    #[inline]
-    async fn clear(&self) {
-        if let Some(state) = &*self.0.read().await {
-            state.send(None).await.unwrap();
-        }
-    }
-}
 
 /// Loads the current `password` into storage from the UI.
 #[tauri::command]
-async fn load_password(state: State<'_, Password>, password: String) -> Result<(), ()> {
+async fn load_password(state: State<'_, PasswordStore>, password: String) -> Result<(), ()> {
     state.load(password).await;
     Ok(())
 }
 
 /// Removes the current password from storage.
 #[tauri::command]
-async fn clear_password(state: State<'_, Password>) -> Result<(), ()> {
+async fn clear_password(state: State<'_, PasswordStore>) -> Result<(), ()> {
     state.clear().await;
     Ok(())
 }
 
 /// Connection Event
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 #[serde(rename_all = "kebab-case")]
 enum ConnectEvent {
     /// Create Account
@@ -153,7 +57,9 @@ async fn connect(window: Window) -> ConnectEvent {
     match account_exists().await {
         Ok(true) => ConnectEvent::SetupAuthorization,
         _ => {
-            show(&window);
+            window.set_always_on_top(true).unwrap();
+            window.center().unwrap();
+            window.show().unwrap();
             ConnectEvent::CreateAccount
         }
     }
@@ -161,37 +67,22 @@ async fn connect(window: Window) -> ConnectEvent {
 
 /// Sends the mnemonic to the UI for the user to memorize.
 #[tauri::command]
-async fn get_mnemonic(password: String) -> String {
-    create_account(password)
-        .await
-        .expect("Unable to create account.")
-        .into_phrase()
+async fn get_mnemonic(password: String) -> Option<String> {
+    Some(create_account(password).await.ok()?.into_phrase())
 }
 
 /// Ends the first round of communication between the UI and the signer.
 #[tauri::command]
-async fn end_connect(state: State<'_, Password>, window: Window) -> Result<(), ()> {
-    hide(&window);
+async fn end_connect(state: State<'_, PasswordStore>, window: Window) -> Result<(), ()> {
+    window.hide().unwrap();
     state.clear().await;
     Ok(())
 }
 
-/// Shows the given `window`.
-#[inline]
-fn show(window: &Window) {
-    window.set_always_on_top(true).unwrap();
-    window.center().unwrap();
-    window.show().unwrap();
-}
-
-/// Hides the given `window`.
-#[inline]
-fn hide(window: &Window) {
-    window.hide().unwrap()
-}
-
 /// Runs the main Tauri application.
 fn main() {
+    const WALLET_FRONTEND_URL: &str = "http://localhost:8181";
+
     let mut app = tauri::Builder::default()
         .system_tray(
             SystemTray::new()
@@ -201,33 +92,15 @@ fn main() {
             SystemTrayEvent::MenuItemClick { id, .. } if id == "exit" => app.exit(0),
             _ => {}
         })
-        .manage(Password::default())
+        .manage(PasswordStore::default())
         .setup(|app| {
             let window = app.get_window("main").unwrap();
-            let state = app.state::<Password>().0.clone();
-            tauri::async_runtime::spawn(async move {
-                let (password_sender, mut password_receiver) = channel(1);
-                Password::setup(state, password_sender).await;
-                password_receiver.recv().await;
-                loop {
-                    match load_request().await {
-                        Some(summary) => window.emit("authorize", summary).unwrap(),
-                        _ => continue,
-                    }
-                    show(&window);
-                    if let Some(password) = password_receiver.recv().await {
-                        match password {
-                            Some(password) => {
-                                send_authorized(generate_proof_payload(password)).await;
-                                hide(&window);
-                            }
-                            _ => {
-                                hide(&window);
-                                send_declined().await
-                            }
-                        }
-                    }
-                }
+            let password_store = app.state::<PasswordStore>().handle();
+            spawn(async move {
+                Service::build(window, password_store.setup().await)
+                    .serve(WALLET_FRONTEND_URL)
+                    .await
+                    .unwrap();
             });
             Ok(())
         })
@@ -239,7 +112,7 @@ fn main() {
             load_password,
         ])
         .build(tauri::generate_context!())
-        .expect("Error while building tauri application.");
+        .expect("Error while building UI.");
 
     #[cfg(target_os = "macos")]
     app.set_activation_policy(tauri::ActivationPolicy::Accessory);
