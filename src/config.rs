2d4a357e042bc63d202afd3b879b315d30d7f561
Kevin Gislason <kevingislason@gmail.com>
2021-11-17 01:22:15 -0500

    updte origin url

--- src/config.rs
+++ src/config.rs
@@ -69,7 +69,7 @@ impl Config {
             root_seed_file: file(dirs_next::config_dir(), "root_seed.aes")?,
             proving_key_directory: directory(dirs_next::data_local_dir())?,
             service_url: String::from("http://127.0.0.1:29987"),
-            origin_url: String::from("http://127.0.0.1:8000"),
+            origin_url: String::from("http://localhost:8000"),
         })
     }
 
