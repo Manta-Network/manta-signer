3fb166a41b6bfe2d3cfa2daab40e7ac5c2b17276
Kevin Gislason <kevingislason@gmail.com>
2022-02-01 22:09:17 -0500

    debug node location;

--- .github/workflows/e2e/build_node.sh
+++ .github/workflows/e2e/build_node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 COMMIT_HASH=$1
-
+pwd
 cd ~
 git clone https://github.com/Manta-Network/Manta.git
 cd Manta
+pwd
 git checkout $COMMIT_HASH
 chmod +x scripts/init.sh
 ./scripts/init.sh
