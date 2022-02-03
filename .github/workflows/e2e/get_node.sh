9c3770beb3891710be905a4f9e55c4fdd45b885f
Kevin Gislason <kevingislason@gmail.com>
2022-02-03 18:44:57 -0500

    get node from S3

--- .github/workflows/e2e/get_node.sh
+++ .github/workflows/e2e/get_node.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-# COMMIT_HASH=$1
-# cd ~
-# git clone https://github.com/Manta-Network/Manta.git
-# cd Manta
-# git checkout $COMMIT_HASH
-# chmod +x scripts/init.sh
-# ./scripts/init.sh
-# cargo build --release
-# chmod +x ~/Manta/target/release/manta
+
+cd ~
 mkdir Manta
 cd Manta
-curl https://manta-ops.s3.amazonaws.com/Dolphin-v0.2.1-841eb2f-x86_64-linux-gnu/manta --output dolphin
-chmod +x dolphin
+curl https://manta-ops.s3.amazonaws.com/Dolphin-v0.2.1-841eb2f-x86_64-linux-gnu/manta --output manta
+chmod +x manta
