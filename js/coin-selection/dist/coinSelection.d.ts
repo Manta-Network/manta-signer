ee7382d842d6d17b55abfbbd9e918b28287354f5
Kevin Gislason <kevingislason@gmail.com>
2022-02-03 19:38:50 -0500

    remove dists

--- js/coin-selection/dist/coinSelection.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CoinSelection {
-    constructor(coins: any, totalValueAtomicUnits: any, targetValueAtomicUnits: any, assetId: any);
-    numberOfZeroCoinsRequired(): number;
-    last(): any;
-    secondLast(): any;
-}
-//# sourceMappingURL=coinSelection.d.ts.map
--- js/coin-selection/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { CoinSelection } from './coinSelection.js';
-import { selectCoins } from './selectCoins.js';
-export { CoinSelection, selectCoins };
-//# sourceMappingURL=index.d.ts.map
