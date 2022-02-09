f21333f1cc254305209c8f9d66c91d9e61fcc968
Kevin Gislason <kevingislason@gmail.com>
2022-02-09 15:46:40 -0500

    debug proving key location

--- .github/workflows/e2e/move_proving_keys.sh
+++ .github/workflows/e2e/move_proving_keys.sh
@@ -3,5 +3,7 @@
 mkdir ~/.local
 mkdir ~/.local/share
 mkdir ~/.local/share/manta-signer
+pwd
+ls
 cp ./sdk/zkp/reclaim_pk.bin ~/.local/share/manta-signer
 cp ./sdk/zkp/transfer_pk.bin ~/.local/share/manta-signer
