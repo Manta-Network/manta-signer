633100f21d9ce63b4a44610478090ef2df4ed335
TJ Sharp <tj.tommyj@protonmail.com>
2022-02-25 04:27:36 -0800

    Fix versioning

--- check_ref.sh
+++ check_ref.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [[ "x$1" == "xmaster" ]]
+echo $BRANCH $TARGET $VERSION $SUFFIX
+
+if [[ "$BRANCH" == "master" ]] && [[ "$TARGET" == "win64" ]]
 then
-    echo '::set-output name=BETA_PREFIX::'
+    echo "::set-output name=SUFFIXED::$VERSION.$SUFFIX"
+elif [[ "$BRANCH" == "master" ]]
+then
+    echo "::set-output name=SUFFIXED::$VERSION-$SUFFIX"
+elif [[ "$TARGET" == "win64" ]]
+then
+    echo "::set-output name=SUFFIXED::$VERSION.$((100 + $SUFFIX))"
 else
-    echo '::set-output name=BETA_PREFIX::beta'
+    echo "::set-output name=SUFFIXED::$VERSION-$((100 + $SUFFIX))"
 fi
