92e6e84c56f5024b47d6ce6160958a58560b1817
TJ Sharp <tjsharp@TJs-MacBook-Pro.local>
2022-03-16 06:38:39 -0400

    Origin

--- test-socket/index.js
+++ test-socket/index.js
@@ -1,5 +1,10 @@
 const WebSocket = require('ws').WebSocket;
-var recparams = require('./recparams.json');
+var recover_account = require('./recover-account.json');
+var derive_shielded_address = require('./derive-shielded-address.json');
+var generate_asset = require('./generate-asset.json');
+var generate_mint_data = require('./generate-mint-data.json');
+var generate_private_transfer_data = require('./generate-private-transfer-data.json');
+var generate_reclaim_data = require('./generate-reclaim-data.json');
 
 var client = new WebSocket('ws://localhost:29987/ws');
 
@@ -13,7 +18,12 @@ client.on('open', function() {
     function sendNumber() {
         if (client.readyState === client.OPEN) {
             //client.send(JSON.stringify("version"));
-            client.send(JSON.stringify({"recover-account": recparams}));
+            client.send(JSON.stringify({"recover-account": recover_account}));
+            //client.send(JSON.stringify({"derive-shielded-address": derive_shielded_address}));
+            //client.send(JSON.stringify({"generate-asset": generate_asset}));
+            //client.send(JSON.stringify({"generate-mint-data": generate_mint_data}));
+            //client.send(JSON.stringify({"generate-private-transfer-data": generate_private_transfer_data}));
+            //client.send(JSON.stringify({"generate-reclaim-data": generate_reclaim_data}));
             setTimeout(sendNumber, 1000);
         }
     }
