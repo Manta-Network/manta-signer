0f150e1633bf7af4da93b9aadef7982144ab2c6d
TJ Sharp <tj.tommyj@gmail.com>
2022-04-22 12:15:58 -0400

    Get a start on wallet integration

--- js/e2e/test/utils.ts
+++ js/e2e/test/utils.ts
@@ -1,3 +1,4 @@
+import Api from 'manta-wasm-wallet-api';
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import { waitReady } from '@polkadot/wasm-crypto'
 import { DolphinTypes } from 'dolphin-api'
@@ -5,5 +6,38 @@ import { DolphinTypes } from 'dolphin-api'
 export async function initDolphinApi (url: string): Promise<ApiPromise > {
   await waitReady()
   const wsProvider = new WsProvider(url)
-  return ApiPromise.create({ provider: wsProvider, types: DolphinTypes })
+  return ApiPromise.create({ provider: wsProvider, types: DolphinTypes });
+}
+
+export async function getUserWallet(url, polkadotJsApi, polkadotJsSigner) {
+    const {
+        Wallet,
+        Signer,
+        PolkadotJsLedger,
+        Transaction,
+        Asset,
+        AssetId,
+        ReceivingKeyRequest
+    } = await import('manta-wasm-wallet');
+
+    const wasmApi = new Api(polkadotJsApi, polkadotJsSigner);
+    const signer = new Signer(url);
+    const ledger = new PolkadotJsLedger(wasmApi);
+    let wallet = new Wallet(ledger, signer);
+
+    wasmApi.setTxResHandler(({ status, events }) => {
+        if (status.isFinalized) {
+            for (const event of events) {
+                if (polkadotJsApi.events.utility.BatchInterrupted.is(event.event)) {
+                    console.log('TODO: failure')
+                    // handle failed tx here
+                } else if (polkadotJsApi.events.utility.BatchCompleted.is(event.event)) {
+                    console.log('TODO: success')
+                    // handle successful tx here
+                }
+            }
+        }
+    });
+
+    return wallet;
 }
