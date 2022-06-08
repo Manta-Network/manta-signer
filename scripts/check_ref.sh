1e8d1fe2b48282c5ddad07f048982703e0bfd7c8
TJ Sharp <tj.tommyj@protonmail.com>
2022-06-08 19:31:51 -0400

    Fix windows string

--- scripts/check_ref.sh
+++ scripts/check_ref.sh
@@ -2,15 +2,9 @@
 
 echo $BRANCH $TARGET $VERSION $SUFFIX
 
-if [[ "$BRANCH" == "master" ]] && [[ "$TARGET" == "win64" ]]
-then
-    echo "::set-output name=SUFFIXED::$VERSION.$SUFFIX"
-elif [[ "$BRANCH" == "master" ]]
+if [[ "$BRANCH" == "master" ]]
 then
     echo "::set-output name=SUFFIXED::$VERSION-$SUFFIX"
-elif [[ "$TARGET" == "win64" ]]
-then
-    echo "::set-output name=SUFFIXED::$VERSION.$((100 + $SUFFIX))"
 else
     echo "::set-output name=SUFFIXED::$VERSION-$((100 + $SUFFIX))"
 fi
