e213e5cbd529a5b1fd93b8ecf24f2200c77eaac2
TJ Sharp <tj.tommyj@protonmail.com>
2022-06-08 20:51:49 -0400

    Script

--- scripts/check_ref.sh
+++ scripts/check_ref.sh
@@ -6,12 +6,11 @@ if [[ "$BRANCH" == "master" ]] && [[ "$TARGET" == "win64" ]]
 then
     echo "::set-output name=SUFFIXED::$VERSION.$SUFFIX"
 elif [[ "$BRANCH" == "master" ]]
-if [[ "$BRANCH" == "master" ]]
 then
     echo "::set-output name=SUFFIXED::$VERSION-$SUFFIX"
 elif [[ "$TARGET" == "win64" ]]
 then
-   echo "::set-output name=SUFFIXED::$VERSION.$((100 + $SUFFIX))"
+    echo "::set-output name=SUFFIXED::$VERSION.$((100 + $SUFFIX))"
 else
     echo "::set-output name=SUFFIXED::$VERSION-$((100 + $SUFFIX))"
 fi
