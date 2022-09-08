a94dfd2333ddfe86d3f694164378a9aaf117a912
Alex <alexparshh@gmail.com>
2022-09-08 17:20:43 -0400

    seed phrase validation

--- ui/src/pages/Recover.js
+++ ui/src/pages/Recover.js
@@ -1,17 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button, Input, Label, Header } from 'semantic-ui-react';
 
 const Recover = (props) => {
 
   const [mnemonics, setMnemonics] = useState('');
+  const [validity, setValidity] = useState(false);
+  const VALID_LENGTH = 24;
 
   const onClickStartRecover = async () => {
-    
+
   }
 
   const onClickCancel = async () => {
     props.cancelRecover();
   }
+
+  const validateMnemonics = () => {
+    let mnemonics_length = mnemonics.split(" ").length;
+
+    if (mnemonics_length == VALID_LENGTH && (!validity)) {
+      setValidity(true);
+    } else if ((mnemonics_length != VALID_LENGTH) && (validity)) {
+      setValidity(false);
+    }
+  }
+
+  useEffect(() => {
+    validateMnemonics();
+  }, [mnemonics])
 
   return (<>
     <Header>
@@ -20,12 +36,18 @@ const Recover = (props) => {
 
     <Input
       placeholder="Input Mnemonics"
-      onChange={(e) => setMnemonics(e.target.value)} 
+      onChange={(e) => {
+        setMnemonics(e.target.value);
+      }}
     />
 
-    <Button primary className="button" onClick={onClickStartRecover}>Start</Button>
-    <Button secondary className="button" onClick={onClickCancel}>Cancel</Button>
-
+    <div>
+      {validity ?
+        <Button primary className="button" onClick={onClickStartRecover}>Start</Button> :
+        <Button disabled primary className="button" onClick={onClickStartRecover}>Start</Button>}
+      <Button secondary className="button" onClick={onClickCancel}>Cancel</Button>
+    </div>
+    {(!validity && mnemonics.length != 0) ? <Label color='red'>Invalid Seed Phrase!</Label> : null}
   </>);
 }
 
