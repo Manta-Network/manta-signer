afd95ea3051070ba7a6b232ba9db2fe3f540b2ce
Apokalip <simeon@manta.network>
2023-01-26 17:09:40 +0200

    push version to 1.0.3
    Signed-off-by: Apokalip <simeon@manta.network>

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -78,10 +78,10 @@ function App() {
           onclick="window.fathom.trackGoal('KUBL03QU', 0);"
           href={
             os === 'Mac'
-              ? 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.1/manta-signer-macos-latest_1.0.1-103_x64.dmg'
+              ? 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.3/manta-signer-macos-latest_1.0.3-103_x64.dmg'
               : os === 'Windows'
-              ? 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.1/manta-signer-windows-2019_1.0.1_x64.msi'
-              : 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.1/manta-signer-ubuntu-18.04_1.0.1_amd64.deb'
+              ? 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.3/manta-signer-windows-2019_1.0.3_x64.msi'
+              : 'https://github.com/Manta-Network/manta-signer/releases/download/v1.0.3/manta-signer-ubuntu-18.04_1.0.3_amd64.deb'
           }
           title={
             os === 'Mac'
